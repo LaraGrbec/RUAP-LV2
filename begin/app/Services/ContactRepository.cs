bd7ab741423d3bf8ca11c2d6e76cbed287307943
LaraGrbec <lara.grbec@gmail.com>
2017-04-04 14:27:01 +0200

    fifth.2

--- begin/app/Services/ContactRepository.cs
+++ begin/app/Services/ContactRepository.cs
@@ -55,6 +55,30 @@ namespace app.Services
             }
         }
 
+        public bool SaveContact(Contact contact)
+        {
+            var ctx = HttpContext.Current;
+
+            if (ctx != null)
+            {
+                try
+                {
+                    var currentData = ((Contact[])ctx.Cache[CacheKey]).ToList();
+                    currentData.Add(contact);
+                    ctx.Cache[CacheKey] = currentData.ToArray();
+
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                    return false;
+                }
+            }
+
+            return false;
+        }
+
     }
 
 }
