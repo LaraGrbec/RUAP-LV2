53d81f020e653f4c6a05d6c4c0997ef72c8351bc
LaraGrbec <lara.grbec@gmail.com>
2017-04-04 14:24:31 +0200

    third.2

--- begin/app/Services/ContactRepository.cs
+++ begin/app/Services/ContactRepository.cs
@@ -28,6 +28,31 @@ namespace app.Services
         };
         }
 
+        public ContactRepository()
+        {
+            var ctx = HttpContext.Current;
+
+            if (ctx != null)
+            {
+                if (ctx.Cache[CacheKey] == null)
+                {
+                    var contacts = new Contact[]
+            {
+                new Contact
+                {
+                    Id = 1, Name = "Glenn Block"
+                },
+                new Contact
+                {
+                    Id = 2, Name = "Dan Roth"
+                }
+            };
+
+                    ctx.Cache[CacheKey] = contacts;
+                }
+            }
+        }
+
     }
 
 }
